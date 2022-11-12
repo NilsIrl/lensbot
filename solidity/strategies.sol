1ba5745c41d34185a3d93a04cdfc222ba78054a5
jackcantdrive <rabbitmoveup@gmail.com>
2022-11-12 22:23:17 +0000

    add Toggle and TitForTat strategies

--- solidity/strategies.sol
+++ solidity/strategies.sol
@@ -18,3 +18,28 @@ contract AlwaysDefect is Bot {
         return true;
     }
 }
+
+contract Toggle is Bot {
+    constructor(uint256 owner_profile) Bot (owner_profile) {}
+
+    function play(
+        address opponent,
+        bool[] calldata previous_plays,
+        uint256 turn_count) external override returns (bool) {
+            return turn_count % 2 == 0;
+    }
+}
+
+contract TitForTat is Bot {
+    constructor(uint256 owner_profile) Bot (owner_profile) {}
+
+    function play(
+        address opponent,
+        bool[] calldata previous_plays,
+        uint256 turn_count) external override returns (bool) {
+            if (previous_plays.length == 0) {
+                return false;
+            }
+            return previous_plays[previous_plays.length - 1];
+    }
+}
