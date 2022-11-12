28404475c67746d333c50b2da884711ccbfe9222
Nils <nils@nilsand.re>
2022-11-12 20:45:08 +0000

    add solidity

---
+++ solidity/prisoners_dilemma/dilemma.sol
@@ -0,0 +1,81 @@
+contract Dilemma is Challenge {
+    mapping (address => int256) public scores;
+    mapping (address => mapping (address => bool[])) public plays;
+    address[] bots;
+    LensBot lensbot;
+    uint256 ownerprofile;
+
+    constructor(address lensbot_addr, uint256 owner_profile) {
+        lensbot = LensBot(lensbot_addr);
+        lensbot.register();
+        ownerprofile = owner_profile;
+    }
+
+    function register() public {
+        Bot botb = Bot(msg.sender);
+
+        for (uint256 i = 0; i < bots.length; ++i) {
+            Bot bota = Bot(bots[i]);
+
+            for (uint256 turn = 0; turn < 5; ++turn) {
+                bool bota_play = bota.play(
+                    msg.sender,
+                    plays[msg.sender][address(bota)],
+                    turn
+                );
+                bool botb_play = botb.play(
+                    address(bota),
+                    plays[address(bota)][msg.sender],
+                    turn
+                );
+
+                // CC -1 -1 
+                // CD -3  0
+                // DD -2 -2
+                // both defect
+                if (bota_play && botb_play) {
+                    scores[address(bota)] -= 2;
+                    scores[address(botb)] -= 2;
+                // bota defect
+                // botb cooperate
+                } else if (bota_play && (!botb_play)) {
+                    scores[address(botb)] -= 3;
+                // bota cooperate
+                // botb defect
+                } else if ((!bota_play) && botb_play) {
+                    scores[address(bota)] -= 3;
+                // both cooperate
+                } else {
+                    scores[address(bota)] -= 1;
+                    scores[address(botb)] -= 1;
+                }
+            }
+        }
+
+        bots.push(address(botb));
+    }
+
+    function getOwnerprofile() view external returns (uint256) {
+        return ownerprofile;
+    }
+
+    function getLeaderboardAt(uint256 i) view external returns (address) {
+        return bots[i];
+    }
+
+    function getPoints(address bot) view external returns (int256) {
+        return scores[bot];
+    }
+
+    function getBotCount() view external returns (uint256) {
+        return bots.length;
+    }
+}
+
+interface Bot {
+    function getOwnerprofile() view external returns (uint256);
+
+    // true is defect
+    // false is cooperate
+    function play(address opponent, bool[] calldata previous_plays, uint256 turn_count) external returns (bool);
+}
