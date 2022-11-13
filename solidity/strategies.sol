4b17bfddd3b5f1d45532b8885e853ccabe632bfc
Nils <nils@nilsand.re>
2022-11-13 02:49:41 +0000

    remove warnings

--- solidity/strategies.sol
+++ solidity/strategies.sol
@@ -8,9 +8,9 @@ contract AlwaysCooperate is Bot {
     constructor(uint256 owner_profile, string memory name) Bot (owner_profile, name) {}
     
     function play(
-    address opponent,
-    bool[] calldata opponent_previous_plays,
-    bool[] calldata bot_previous_plays) external override returns (bool) {
+        address,
+        bool[] calldata,
+        bool[] calldata) external pure override returns (bool) {
         return false;
     }
 }
@@ -20,9 +20,9 @@ contract AlwaysDefect is Bot {
     constructor(uint256 owner_profile, string memory name) Bot (owner_profile, name) {}
 
     function play(
-        address opponent,
-        bool[] calldata opponent_previous_plays,
-        bool[] calldata bot_previous_plays) external override returns (bool) {
+        address,
+        bool[] calldata,
+        bool[] calldata) external pure override returns (bool) {
             return true;
     }
 }
@@ -31,9 +31,9 @@ contract Toggle is Bot {
     constructor(uint256 owner_profile, string memory name) Bot (owner_profile, name) {}
 
     function play(
-        address opponent,
-        bool[] calldata opponent_previous_plays,
-        bool[] calldata bot_previous_plays) external override returns (bool) {
+        address,
+        bool[] calldata,
+        bool[] calldata bot_previous_plays) external pure override returns (bool) {
             return bot_previous_plays.length % 2 == 0;
     }
 }
@@ -42,9 +42,9 @@ contract TitForTat is Bot {
     constructor(uint256 owner_profile, string memory name) Bot (owner_profile, name) {}
 
     function play(
-        address opponent,
+        address,
         bool[] calldata opponent_previous_plays,
-        bool[] calldata bot_previous_plays) external override returns (bool) {
+        bool[] calldata) external pure override returns (bool) {
             if (opponent_previous_plays.length == 0) {
                 return true;
             }
