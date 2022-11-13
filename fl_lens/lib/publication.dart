9b83441d3584d17d83507aac33909b856c154200
Nils <nils@nilsand.re>
2022-11-13 11:42:17 +0000

    upload

--- fl_lens/lib/publication.dart
+++
@@ -1,8 +0,0 @@
-// ignore_for_file: invalid_annotation_target
-
-import 'package:flutter/material.dart';
-import 'package:json_annotation/json_annotation.dart';
-import 'package:vrouter/vrouter.dart';
-import 'networking.dart';
-//part 'publication.g.dart';
-
--- fl_lens/w.js
+++
@@ -1,5 +0,0 @@
-// log into metamask
-
-if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider);
-}
