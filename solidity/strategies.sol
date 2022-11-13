2c9a41daec4ad77019df983b3dc4635c50d2cce5
Nils <nils@nilsand.re>
2022-11-13 11:31:17 +0000

    add advanced bot

--- solidity/strategies.sol
+++ solidity/strategies.sol
@@ -51,3 +51,23 @@ contract TitForTat is Bot {
             return opponent_previous_plays[opponent_previous_plays.length - 1];
     }
 }
+
+contract Advanced is Bot {
+    constructor(uint256 owner_profile, string memory name) Bot (owner_profile, name) {}
+
+    // true is defect
+    // false is cooperate
+    function play(
+        address opponent,
+        bool[] calldata opponent_previous_plays,
+        bool[] calldata bot_previous_plays
+    ) external override virtual returns (bool) {
+        bool[] memory my_plays_minus1 = new bool[](opponent_previous_plays.length - 1);
+        bool[] memory their_plays_minus1 = new bool[](opponent_previous_plays.length - 1);
+        for (uint256 i = 0; i < opponent_previous_plays.length - 1; i += 1) {
+            my_plays_minus1[i] = bot_previous_plays[i];
+            their_plays_minus1[i] = opponent_previous_plays[i];
+        }
+        return Bot(opponent).play(address(this), my_plays_minus1, their_plays_minus1);
+    }
+}
