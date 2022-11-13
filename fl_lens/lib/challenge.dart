af68490ee31c9a4e6004536cf886e4c8393f22ae
Max Smith <smithmaxhuman+git@gmail.com>
2022-11-13 00:01:26 +0000

    Add data structure for Challenge

--- fl_lens/lib/challenge.dart
+++ fl_lens/lib/challenge.dart
@@ -2,6 +2,7 @@ import 'state.dart' as s;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+/*
 class ChallengePage extends StatelessWidget {
   const ChallengePage({super.key});
 
@@ -10,8 +11,38 @@ class ChallengePage extends StatelessWidget {
     final state = context.read<s.State>();
 
     final challenges = state.getChallengeOwnderIDs();
-    
 
     return const ListView.builder(itemBuilder: );
   }
+}
+*/
+
+class Challenge {
+  final int botCount;
+  final Map<String, int> botScores;
+  final String owner;
+
+  const Challenge({
+    required this.botCount,
+    required this.botScores,
+    required this.owner,
+  });
+
+  List<LeaderboardEntry> getLeaderboard() {
+    List<LeaderboardEntry> leaderboard = [];
+    botScores.forEach(
+        (k, v) => leaderboard.add(LeaderboardEntry(addr: k, score: v)));
+    leaderboard.sort((a, b) => b.score.compareTo(a.score));
+    return leaderboard;
+  }
+}
+
+class LeaderboardEntry {
+  final String addr;
+  final int score;
+
+  const LeaderboardEntry({
+    required this.addr,
+    required this.score,
+  });
 }
