88acce2c49794ed10c2eb4e11f7937f07f817359
Nils <nils@nilsand.re>
2022-11-13 12:04:56 +0000

    stratteoishuf

--- solidity/strategies.sol
+++ solidity/strategies.sol
@@ -62,12 +62,6 @@ contract Advanced is Bot {
         bool[] calldata opponent_previous_plays,
         bool[] calldata bot_previous_plays
     ) external override virtual returns (bool) {
-        bool[] memory my_plays_minus1 = new bool[](opponent_previous_plays.length - 1);
-        bool[] memory their_plays_minus1 = new bool[](opponent_previous_plays.length - 1);
-        for (uint256 i = 0; i < opponent_previous_plays.length - 1; i += 1) {
-            my_plays_minus1[i] = bot_previous_plays[i];
-            their_plays_minus1[i] = opponent_previous_plays[i];
-        }
-        return Bot(opponent).play(address(this), my_plays_minus1, their_plays_minus1);
+        return Bot(opponent).play(address(this), bot_previous_plays, opponent_previous_plays);
     }
 }
